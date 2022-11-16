80eec71ce78016c707f630ee4486475d54e116d6
VictorAriton <victorar@email.unc.edu>
2022-11-15 22:36:04 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -27,3 +27,28 @@ const latitude = args.n || args.s * -1;
 
 const longitude = args.e || args.w * -1;
 
+
+var day = 1;
+
+if (args.d != undefined) {
+	day = args.d;
+}
+
+const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_hours&current_weather=true&timezone=" + timezone;
+const response = await fetch(url);
+const data = await response.json();
+
+if (args.j) {
+	console.log(data);
+	process.exit(0);
+}
+
+
+if (day == 0) {
+    console.log(printing + "today.")
+  } else if (day > 1) {
+    console.log(printing + "in " + day + " days.")
+  } else {
+    console.log(printing + "tomorrow.")
+  }
+  
